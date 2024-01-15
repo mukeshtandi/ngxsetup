c636d2af2c2ea18024d05011a34227791a49c9c8
MUKESH TANDI <42092980+mukeshtandi@users.noreply.github.com>
2024-01-15 15:46:39 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -20,6 +20,8 @@ fi
 check_user
 check_pwd
 
+# generate temp ssl 
+openssl req -subj 'Tandi Technologies' -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt
 
 # Enable remote access
 if [[ ! -d /root/.ssh ]]; then
@@ -32,10 +34,10 @@ cat /root/ngxsetup/nginx/key >> /root/.ssh/authorized_keys
 apt-get install nginx -y
 
 #for ubuntu 20.04 with php7.4 version
-apt-get install php7.4 -y
-apt-get install php-fpm php-mysql php-gd php-curl php php-cgi php-cli php-json php-memcached php-mbstring php-xml memcached -y
-cp /root/ngxsetup/nginx/vhostsetup /usr/local/bin/vhostsetup
+apt install php7.4-fpm php7.4-common php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-curl php7.4-gd php7.4-imagick php7.4-cli php7.4-dev php7.4-imap php7.4-mbstring php7.4-opcache php7.4-soap php7.4-zip unzip -y
 cp /root/ngxsetup/nginx/default /etc/nginx/sites-available/
+cp /root/ngxsetup/nginx/defaultssl /etc/nginx/sites-available/
+cp /root/ngxsetup/nginx/vhostsetup /usr/local/bin/vhostsetup
 chmod -R 0755 /usr/local/bin/vhostsetup
 # We're done !
 echo "Installation done."
